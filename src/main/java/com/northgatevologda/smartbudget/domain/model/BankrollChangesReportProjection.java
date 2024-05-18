200a6ae7e708f625f57747ff56c77766b140ebc3
AAKVAS <alexej_plot@mail.ru>
2024-05-18 17:00:12 +0300

    Сделал отчёт о изменениях по счетам

---
+++ src/main/java/com/northgatevologda/smartbudget/domain/model/BankrollChangesReportProjection.java
@@ -0,0 +1,14 @@
+package com.northgatevologda.smartbudget.domain.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public interface BankrollChangesReportProjection {
+    Integer getTransactionTypeId();
+    Instant getPeriodStart();
+    BigDecimal getAmount();
+
+    Integer INCOME_TYPE_ID = 1;
+    Integer EXPENSES_TYPE_ID = 2;
+    Integer BALANCE_TYPE_ID = 0;
+}
